482cc2d4012bd7e0441f54a51a4597af6d4f300a
Pablo Carbonell <pablocar80@gmail.com>
2019-09-23 10:35:33 -0500

    added DEBUG configuration for webpack

--- src/LaraClient/webpack.config.js
+++ src/LaraClient/webpack.config.js
@@ -1,13 +1,24 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const webpack = require('webpack');
+const fs = require('fs');
 
 const banner = 'Lara Web Engine\n'
     + 'Copyright (c) 2019 Integrative Software LLC.\n'
     + 'License: Apache-2.0';
 
+var data = fs.readFileSync('../environment.txt', 'utf8');
+var configuration = data.toString().trim();
+var buildMode = "production";
+if (configuration === "Debug") {
+    buildMode = "development";
+}
+console.log("configuration: " + configuration);
+console.log("mode: " + buildMode);
+
 module.exports = {
     entry: './src/index.ts',
+    mode: buildMode,
     plugins: [
         new CleanWebpackPlugin(),
         new webpack.BannerPlugin(banner),
